1d1e9fe87e17aaf10d7e3cba7fe86361f841a2a3
MJARuijs <mja.ruijs@gmail.com>
2025-11-14 08:02:04 +0100

    Add .config files + an install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+source_dir=~/.config/marc-config/.config
+destination_dir=~/.config/
+
+entries=("$source_dir"/*)
+
+for entry in ${entries[@]}; do
+  file_name="${entry##*/}"
+
+  dest=$destination_dir$file_name
+
+  if [[ ! -d $dest || -z "$(ls -A $dest)" && ! -f $dest ]]; then
+    $(rmdir $dest)
+    ln -s $entry $destination_dir
+  fi
+done
