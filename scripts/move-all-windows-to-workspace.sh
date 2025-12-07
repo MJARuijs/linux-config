fc938e32940bece18c0ece1a512480dfb911e124
MJARuijs <mja.ruijs@gmail.com>
2025-12-07 10:48:48 +0100

    Make move-all-windows-to-workspace work regardless of monitor-count

--- scripts/move-all-windows-to-workspace.sh
+++ scripts/move-all-windows-to-workspace.sh
@@ -5,14 +5,14 @@ monitor_count=$(hyprctl monitors all | grep -o Monitor | wc -l)
 
 #echo $current_workspace
 
-target_workspace=$(((($1 - 1) * 5) + $current_workspace % $monitor_count))
+target_workspace=$(((($1 - 1) * $monitor_count) + $current_workspace % $monitor_count))
 #echo $target_workspace
 
 #new_monitor=$(($current_monitor + $workspace_offset * $monitor_count))
 
 window_addresses=$(hyprctl clients -j | jq -r ".[] | select(.workspace.id == $current_workspace) | .address")
 for address in $window_addresses; do
-   hyprctl dispatch movetoworkspacesilent "$target_workspace"
+  hyprctl dispatch movetoworkspacesilent "$target_workspace"
 done
 echo $target_workspace
 #hyprctl dispatch workspace "$new_monitor"
