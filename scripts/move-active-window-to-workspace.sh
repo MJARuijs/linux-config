bbbb1863366c5b2bb8b776654782c582694b2602
MJARuijs <mja.ruijs@gmail.com>
2025-12-07 10:47:45 +0100

    Make move-active-window-to-workspace work regardless of monitor-count

--- scripts/move-active-window-to-workspace.sh
+++ scripts/move-active-window-to-workspace.sh
@@ -5,7 +5,7 @@ monitor_count=$(hyprctl monitors all | grep -o Monitor | wc -l)
 
 echo $current_workspace
 
-target_workspace=$(((($1 - 1) * 5) + (($current_workspace - 1) % $monitor_count + 1)))
+target_workspace=$(((($1 - 1) * $monitor_count) + (($current_workspace - 1) % $monitor_count + 1)))
 echo $target_workspace
 
 hyprctl dispatch movetoworkspacesilent "$target_workspace"
