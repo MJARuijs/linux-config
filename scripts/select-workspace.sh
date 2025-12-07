392262c02538426429b6e57c2c6633d2fdfe459f
MJARuijs <mja.ruijs@gmail.com>
2025-12-07 10:45:25 +0100

    Make select-workspace script work regardless of monitor-count

--- scripts/select-workspace.sh
+++ scripts/select-workspace.sh
@@ -1,19 +1,8 @@
 #!/usr/bin/env bash
-target_workspace=$((($1 - 1) * 5))
 monitor_count=$(hyprctl monitors all | grep -o Monitor | wc -l)
-# target_workspace=$((($1 - 1) * $monitor_count))
+target_workspace=$((($1 - 1) * $monitor_count))
 
 for ((i = $monitor_count; i > 0; i--)); do
   echo 'Switching to ' $(($target_workspace + $i))
-  # hyprctl dispatch workspace "$(($target_workspace + $i))"
+  hyprctl dispatch workspace "$(($target_workspace + $i))"
 done
-echo "Alt. " $(($target_workspace + 2))
-echo "Alt. " $(($target_workspace + 3))
-echo "Alt. " $(($target_workspace + 4))
-echo "Alt. " $(($target_workspace + 5))
-echo "Alt. " $(($target_workspace + 1))
-hyprctl dispatch workspace "$(($target_workspace + 2))"
-hyprctl dispatch workspace "$(($target_workspace + 3))"
-hyprctl dispatch workspace "$(($target_workspace + 4))"
-hyprctl dispatch workspace "$(($target_workspace + 5))"
-hyprctl dispatch workspace "$(($target_workspace + 1))"
