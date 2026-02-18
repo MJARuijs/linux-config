f2c244c44e3c745c93bc40526a550714063a34be
MJARuijs <mja.ruijs@gmail.com>
2026-02-19 00:04:32 +0100

    Start messing with colors and improve alacritty font

--- scripts/launch_neovide.sh
+++ scripts/launch_neovide.sh
@@ -1,8 +1,10 @@
 echo $HOME
 echo $(pwd)
 if [ "$(pwd)" == "$HOME" ]; then
-  neovide --fork ~/.config/nvim/
+    neovide --fork ~/.config/nvim/
+elif [ -n "$1" ]; then
+    neovide --fork $1
 else
-  neovide --fork $(pwd)
+    neovide --fork $(pwd)
 fi
-exit;
+exit
