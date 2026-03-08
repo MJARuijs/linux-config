93f2c159157498df17b03923e187ca5244bae0bd
MJARuijs <mja.ruijs@gmail.com>
2026-03-08 12:46:39 +0100

    Improve launch_neovide script

--- scripts/launch_neovide.sh
+++ scripts/launch_neovide.sh
@@ -3,7 +3,7 @@ echo $(pwd)
 if [ -n "$1" ]; then
     neovide --fork $1 $2 $3 $4 $5
 elif [ "$(pwd)" == "$HOME" ]; then
-    neovide --fork ~/.config/nvim/
+    neovide --fork -- --cmd "tcd ~/.config/nvim/"
 else
     neovide --fork $(pwd)
 fi
