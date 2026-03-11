fb6bd0ec9e0421ce2e01a48a6cce7e0f15011d62
MJARuijs <mja.ruijs@gmail.com>
2026-03-11 15:30:08 +0100

    Add script that automatically sets the AngularCompilerOptions

---
+++ scripts/add-angular-copiler-options.lua
@@ -0,0 +1,28 @@
+local module_folder = "/home/marc/linux-config/scripts/"
+package.path = module_folder .. "?.lua;" .. package.path
+local util = require("util")
+
+local FILE_PATH = "/home/marc/Documents/inOne/Development/heidi/ngMyinONE/tsconfig.json"
+
+local file_lines = util.getFileLines(FILE_PATH)
+
+local has_compiler_options = false
+
+for _, line in pairs(file_lines) do
+	if line:trim():startsWith('"angularCompilerOptions"') then
+		has_compiler_options = true
+	end
+end
+
+if has_compiler_options == false then
+	local last_line = table.remove(file_lines, #file_lines)
+
+	--
+	local file_content = ""
+	for _, line in pairs(file_lines) do
+		file_content = file_content .. line .. "\n"
+	end
+	file_content = file_content .. '  "angularCompilerOptions": {\n' .. '    "strictTemplates": true\n' .. "  }\n" .. last_line
+
+	util.write_to_file(FILE_PATH, file_content)
+end
